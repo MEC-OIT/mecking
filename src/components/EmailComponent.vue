fa7cfa4abde45b3715329ae4ceac22dab998d5d8
Vezro <45973533+Vezro@users.noreply.github.com>
2019-01-06 23:20:19 -0500

    Initial commit

---
+++ src/components/EmailComponent.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <q-input
+  class="formBackground moreCenterT"
+  v-model="password"
+  type="password"
+  :before="[{icon: 'lock', handler () {}}]"
+  placeholder="password"
+  :class="{ hasError: v.$error }"
+   />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ""
+    },
+    v: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    password: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.v.$touch();
+        this.$emit("input", value);
+      }
+    }
+  }
+};
+</script>
+
+<style media="screen">
+.moreCenterT{
+  margin-top:-10vh;
+
+}
+</style>
---
+++ src/pages/videoPod.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+
+    <h3>Podcost: President Speaks</h3>
+    <iframe width="100%" height="400" src="https://clyp.it/abmrqywk/widget" frameborder="0"></iframe>
+
+</q-page>
+</template>
+<script type="text/javascript">
+
+</script>
+
+
+<style>
+</style>
